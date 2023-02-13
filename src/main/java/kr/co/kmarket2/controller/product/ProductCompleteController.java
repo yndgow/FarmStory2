26511c1c540a2476a260563e42a7a48f7f7a5ece
alswns3906 <alswns3906@naver.com>
2023-02-13 13:41:10 +0900

    2023/02/13 13:41

--- src/main/java/kr/co/kmarket2/controller/product/ProductCompleteController.java
+++ src/main/java/kr/co/kmarket2/controller/product/ProductCompleteController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProductCompleteController {
 	
-	@GetMapping("list")
-	public String list(){
-		return "list";
+	@GetMapping("complete")
+	public String complete(){
+		return "complete";
 	}
 
 }
