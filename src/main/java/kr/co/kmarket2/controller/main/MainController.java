752a8a7c5c9b9932a2a96d44eed95a1875f981ff
Hongscoding <alswns-3948@naver.com>
2023-02-22 00:44:48 +0900

    index list 완료

--- src/main/java/kr/co/kmarket2/controller/main/MainController.java
+++ src/main/java/kr/co/kmarket2/controller/main/MainController.java
@@ -1,11 +1,35 @@
 package kr.co.kmarket2.controller.main;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.kmarket2.service.ProductService;
+import kr.co.kmarket2.vo.ProductVO;
 
 public class MainController {
 
+	@Autowired
+	private ProductService service;
+	
 	@GetMapping(value={"/", "/index"})
-	public String main() {
+	public String main(Model model) {
+		List<ProductVO> productsbest = service.selectProductsBest();
+		List<ProductVO> productshit = service.selectProductsHit();
+		List<ProductVO> productsscore = service.selectProductsScore();
+		List<ProductVO> productsnew = service.selectProductsNew();
+		List<ProductVO> productsdiscount = service.selectProductsDiscount();
+		
+		model.addAttribute("best", productsbest);
+		model.addAttribute("hit", productshit);
+		model.addAttribute("socore", productsscore);
+		model.addAttribute("new", productsnew);
+		model.addAttribute("discount", productsdiscount);
+		model.addAttribute("imagesEndpoint", "images/");
+		model.addAttribute("filename", "cat-g893543a6c_640.jpg");
+
 		return "/, /index";
 	}
 }
